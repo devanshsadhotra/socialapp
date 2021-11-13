36229bfe3feaef3ae9c978ca6f02360231c26dd0
Devansh <devanshsadhotra@outlook.com>
2021-11-13 18:33:56 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+console.log("hello from D-talks, wassup! ")
+import express from "express";
+const app= express()
+import dateFormat, { masks } from "dateformat";
+const PORT =4000 || process.env.PORT
+
+app.get("/",(req, res)=>{
+    res.status(200).send("<h1>hello from loc</h1>")
+
+})
+
+app.get("/api/v1/instagram",(req,res)=>{
+    const instaSocial={
+        userName:"devanshsadhotra",
+        followers: 1300,
+        follows:800,
+        date:  dateFormat(Date.now(), "dddd, mmmm dS, yyyy, h:MM:ss TT")
+
+    }
+    res.status(200).json(instaSocial)
+})
+app.get("/api/v1/facebook",(req,res)=>{
+    const facebookSocial={
+        userName:"devanshsadhotra",
+        followers: 1200,
+        follows:800,
+        date: dateFormat(Date.now(), "dddd, mmmm dS")
+    }
+    res.status(200).json(facebookSocial)
+})
+app.get("/api/v1/linkedIn",(req,res)=>{
+    const linkedInSocial={
+        userName:"devansh sadhotra",
+        followers: 500,
+        follows:120,
+        date: dateFormat(Date.now(), "dddd, mmmm dS, yyyy,")
+    }
+    res.status(200).json(linkedInSocial)
+})
+app.get("/api/v1/token",(req,res)=>{
+    console.log(req.params.token)
+    res.status(200).json({ param:req.params.token})
+})
+app.listen(PORT,()=>{
+    console.log("server is running at ", PORT )
+})
